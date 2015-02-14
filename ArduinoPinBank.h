03a85412597794204ac51066a32123fcac41b467
Collin Rogowski <collin.rogowski@inovex.de>
2015-02-14 22:24:59 +0100

    added virtual destructors to all classes

--- ArduinoPinBank.h
+++ ArduinoPinBank.h
@@ -2,6 +2,7 @@
 
 class ArduinoPinBank : public PinBank {
 public:
+	virtual ~ArduinoPinBank() {}
 	virtual const int digitalRead(uint8_t pin);
 	virtual const void digitalWrite(uint8_t pin, uint8_t value);
 };
--- MCP23017PinBank.h
+++ MCP23017PinBank.h
@@ -4,6 +4,7 @@
 class MCP23017PinBank : public PinBank {
 public:
 	MCP23017PinBank(Adafruit_MCP23017 mcp);
+	virtual ~MCP23017PinBank() {}
 	virtual const int digitalRead(uint8_t pin);
 	virtual const void digitalWrite(uint8_t pin, uint8_t value);
 private:
