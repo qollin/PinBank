b188376c97db7af5175111701b8680af803edd6f
Collin Rogowski <collin.rogowski@inovex.de>
2015-02-20 21:59:55 +0100

    added push button

--- MCP23017PinBank.h
+++ MCP23017PinBank.h
@@ -5,8 +5,13 @@ class MCP23017PinBank : public PinBank {
 public:
 	MCP23017PinBank(Adafruit_MCP23017 mcp);
 	virtual ~MCP23017PinBank() {}
+	virtual const void setup();
 	virtual const int digitalRead(uint8_t pin);
 	virtual const void digitalWrite(uint8_t pin, uint8_t value);
+	virtual const void addInterruptListener(uint8_t pin, InterruptListener* listener);
+	virtual const void handleInterrupt();
 private:
+	static const int NUMBER_OF_PINS = 16;
 	Adafruit_MCP23017 mcp;
+	InterruptListener* listeners[16];
 };
